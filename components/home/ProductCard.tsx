291aac2a51613b34255576b21f6c9818a7c7bb97
Cicekyolla <tunahancicekci@gmail.com>
2026-07-10 12:17:39 +0300

    Update ProductCard.tsx

--- components/home/ProductCard.tsx
+++ components/home/ProductCard.tsx
@@ -110,7 +110,7 @@ export function ProductCard({ product, idx, contextTag }: { product: Product; id
             src={product.image}
             alt={product.name}
             scale={hovered ? 1.06 : 1}
-            padding="12px"
+            padding="4px"
             derivatives={product.derivatives}
             blurhash={product.blurhash}
             sizes="(max-width:640px) 50vw, (max-width:1024px) 33vw, 25vw"
